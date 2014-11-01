d7f7ecae2ca8a47eb2b722564a4f0f96e2fd5c05
Vitaliy Trofymiuk <vitaliy.trofymiuk@icloud.com>
2014-11-01 22:53:16 +0200

    VT: Adding files from folder recursivly to git repository

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ confetti1-import
 ================
 
 ClearCase-to-git import scripts
+
+Building:
+
+git clone https://github.com/rafie/confetti1-import
+cd confetti1-import
+gem build confetti1-import.gemspec
+gem install confetti1-import-0.0.0.gem
+
+Usage:
+
+confetti < folder_to_added_to_gir > < git repository >
+(confetti /Users/linuxoid/workspace/dev/confetti_test https://github.com/deut/test_repo.git)
+
+TODO:
+1) Not working in Windows, currently fixing it.
+2) Config for ignored files.
+3) Logs.
+4) Tests.
+
--- lib/confetti1/import.rb
+++ lib/confetti1/import.rb
@@ -1,7 +1,51 @@
 require "confetti1/import/version"
+require 'fileutils'
+require "git"
 
 module Confetti1
   module Import
-    # Your code goes here...
+
+    class GitHandler
+      
+      def initialize(cc_view, git_uri, repo_name="our_repo")
+        @cc_veiw_path = cc_view
+        @repo_name = repo_name
+        @git_uri = git_uri
+      end
+
+      def init!
+        Dir.chdir @cc_veiw_path
+        files_end_dirs = Dir.glob("**/*")
+
+        @dirs =  files_end_dirs.select{|f| Dir.exist?("#{@cc_veiw_path}/#{f}")}
+        @files = files_end_dirs.select{ |f| !Dir.exist?("#{@cc_veiw_path}/#{f}") and File.exist?("#{@cc_veiw_path}/#{f}")}
+        
+        @git_repo = Git.clone @git_uri, @repo_name
+        @path_to_repo = "#{@cc_veiw_path}/#{@repo_name}"
+      end
+
+      def add_all
+        Dir.chdir @path_to_repo
+        FileUtils.mkdir_p @dirs
+        @files.each do |view_file|
+          path_to_file = "#{@cc_veiw_path}/#{view_file}"
+          path_to_repo_file = "#{@path_to_repo}/#{view_file}"
+          FileUtils.copy_file(path_to_file, path_to_repo_file)
+          @git_repo.add path_to_repo_file
+        end
+      end
+
+      def commit_and_push!
+        @git_repo.commit('First commit from script!')
+        @git_repo.pull
+        @git_repo.push
+      end 
+
+      def cleanup!
+        FileUtils.rm_rf @path_to_repo
+      end                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+    end
+
   end
-end
+end                                                
