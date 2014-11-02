872ca94918678df1edc76534a5e7afdd1b51de4a
Vitaliy Trofymiuk <vitaliy.trofymiuk@icloud.com>
2014-11-02 14:20:02 +0200

    VT: Fixes and improvements in UNIX

--- lib/confetti1/import.rb
+++ lib/confetti1/import.rb
@@ -17,28 +17,51 @@ module Confetti1
         Dir.chdir @cc_veiw_path
         files_end_dirs = Dir.glob("**/*")
 
-        @dirs =  files_end_dirs.select{|f| Dir.exist?("#{@cc_veiw_path}/#{f}")}
-        @files = files_end_dirs.select{ |f| !Dir.exist?("#{@cc_veiw_path}/#{f}") and File.exist?("#{@cc_veiw_path}/#{f}")}
-        
-        @git_repo = Git.clone @git_uri, @repo_name
-        @path_to_repo = "#{@cc_veiw_path}/#{@repo_name}"
+        @dirs =  files_end_dirs.select{|f| Dir.exist?(File.join(@cc_veiw_path, f))}
+        @files = files_end_dirs.select{ |f| !Dir.exist?(File.join(@cc_veiw_path, f)) and File.exist?(File.join(@cc_veiw_path, f))}
+        begin
+          @git_repo = Git.clone @git_uri, @repo_name
+        rescue Git::GitExecuteError
+          Dir.chdir @cc_veiw_path
+          @git_repo = Git.open @repo_name
+        end
+        @path_to_repo = File.join @cc_veiw_path, @repo_name
+        @any_changes = false 
       end
 
       def add_all
         Dir.chdir @path_to_repo
         FileUtils.mkdir_p @dirs
+
+        add_file = Proc.new do |file, is_new|
+          FileUtils.copy_file(File.join(@cc_veiw_path, file), File.join(@path_to_repo, file)) if is_new
+          @git_repo.add File.join(@path_to_repo, file)
+          @any_changes = true
+        end
+
         @files.each do |view_file|
-          path_to_file = "#{@cc_veiw_path}/#{view_file}"
-          path_to_repo_file = "#{@path_to_repo}/#{view_file}"
-          FileUtils.copy_file(path_to_file, path_to_repo_file)
-          @git_repo.add path_to_repo_file
+          path_to_file = File.join @cc_veiw_path, view_file
+          path_to_repo_file = File.join @path_to_repo, view_file
+
+          if File.exist? path_to_repo_file
+            next unless File.exist? path_to_file
+            next if FileUtils.compare_file(path_to_file, path_to_repo_file)
+            add_file.call(view_file, true)
+          end
+
+          next unless File.exist? path_to_repo_file
+          add_file.call(view_file, false)
         end
       end
 
       def commit_and_push!
-        @git_repo.commit('First commit from script!')
-        @git_repo.pull
-        @git_repo.push
+        #TODO: Shold be replaced to Clear Case metadata
+        if @any_changes
+          @git_repo.commit('First commit from script!')
+          @git_repo.pull
+          @git_repo.push
+          @git_woking_tree_clean = true
+        end
       end 
 
       def cleanup!
