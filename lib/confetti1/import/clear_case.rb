569dc4c11c2f62717320912dd2a8d41befb5dfbf
Vitaliy Trofymiuk <vitaliy.trofymiuk@icloud.com>
2014-11-06 09:14:06 +0200

    WIP: Moving to remote machine

--- lib/confetti1/import/clear_case.rb
+++ lib/confetti1/import/clear_case.rb
@@ -19,15 +19,16 @@ module Confetti1
 
       def initialize
         #FIXME I am ugly hardcoded, fix me, please
-        @view_name = "vtrofymyuk_view1"
-        @view_location = "M:"
+        # @view_name = "vtrofymyuk_view1"
+        # @view_location = "M:"
         #------------------------------------------
-        @view_path = File.join @view_location, @view_name 
-        Dir.chdir @view_path 
+        #@view_path = File.join @view_location, @view_name 
+        #Dir.chdir @view_path 
       end
 
       def configspec
         out = command("ct", "catcs")
+        
       end
 
       def configspec=(args)
@@ -44,7 +45,15 @@ module Confetti1
 
     private
 
-      def parse_configspec
+      def parse_configspec(path="#{Confetti1::Import.root}/test/test_store/mock_configspec")
+        conf_spec = File.read(path)
+        parsed_rows = conf_spec.split("\n").map do |cs| 
+          cs.split.join(" ")
+        end.reject{|cs| cs.empty?}
+        parsed_costum = parsed_rows.map do |r|
+          r.gsub(/\s+/, "\s").split("\s")
+        end
+        parsed_custom
       end
 
       def command(cmd, *argv)
