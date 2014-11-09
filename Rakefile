ffffa85ef3d9826cc55f2cdea95b6c0e52f1eeec
Vitaliy Trofymiuk <vitaliy.trofymiuk@icloud.com>
2014-11-10 00:28:50 +0200

    VT: Configspec parsing

--- Rakefile
+++ Rakefile
@@ -6,3 +6,12 @@ end
 
 desc "Run tests"
 task :default => :test
+
+desc "Build and install gem"
+task :install do
+  require 'confetti1/import' 
+  system "gem uninstall confetti1-import"
+  raise 'Build failed!' unless system('rake test --trace')
+  `gem build confetti1-import.gemspec`
+  `gem install confetti1-import-#{Confetti1::Import.version}.gem`
+end
--- lib/confetti1/import/clear_case.rb
+++ lib/confetti1/import/clear_case.rb
@@ -15,20 +15,18 @@ module Confetti1
     # md d:\gitest\mcu_8.3.0.11.8
     # pushd d:\gitest\mcu_8.3.0.11.8
     # git clone file://%v%/nbu.meta/git
-    class ClearCase
+    class ClearCase < Base
 
       def initialize
         #FIXME I am ugly hardcoded, fix me, please
-        # @view_name = "vtrofymyuk_view1"
-        # @view_location = "M:"
-        #------------------------------------------
-        #@view_path = File.join @view_location, @view_name 
-        #Dir.chdir @view_path 
+        @view_name = confetti_config[:clear_case][:view][:name]
+        @view_location = confetti_config[:clear_case][:view][:disc]
+        @view_path = File.join @view_location, @view_name  
       end
 
       def configspec
         out = command("ct", "catcs")
-        
+        parse_configspec out
       end
 
       def configspec=(args)
@@ -36,7 +34,7 @@ module Confetti1
       end
 
       def mkview
-        out = command("ct1", "mkview", "-raw", "-name", @view_name)
+        out = command("mkview", "-raw", "-name", @view_name)
       end
 
       def mount
@@ -45,21 +43,20 @@ module Confetti1
 
     private
 
-      def parse_configspec(path="#{Confetti1::Import.root}/test/test_store/mock_configspec")
-        conf_spec = File.read(path)
-        parsed_rows = conf_spec.split("\n").map do |cs| 
-          cs.split.join(" ")
-        end.reject{|cs| cs.empty?}
-        parsed_costum = parsed_rows.map do |r|
-          r.gsub(/\s+/, "\s").split("\s")
-        end
-        parsed_custom
+      def vobs_list
+        cs = parse_configspec
+
       end
 
-      def command(cmd, *argv)
-        output = `#{cmd} #{argv.join(" ")}`
-        output.split("\n")
-      end 
+      def parse_configspec(path="#{Confetti1::Import.root}/test/test_store/mock_configspec")
+        conf_spec = File.read(path)
+        clean = conf_spec.split("\n").map{|cs| cs.gsub(/\s+/, " ")}.reject{|cs|cs.size < 2}
+        preparsed = clean.map{|cs| cs.split("\s")}
+        preparsed.shift
+        preparsed.map do |pp|
+          {vob: pp[1].gsub(/(\.){3}|\//,""), version: pp[2]}
+        end
+      end
 
     end
   end
