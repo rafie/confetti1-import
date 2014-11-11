83f542c05794dd8a155f32d44c54536271b65ebe
Vitaliy Trofymiuk <vitaliy.trofymiuk@icloud.com>
2014-11-11 16:25:16 +0200

    VT: Git Status method

--- lib/confetti1/import/confetti_git.rb
+++ lib/confetti1/import/confetti_git.rb
@@ -1,45 +1,48 @@
 module Confetti1
   module Import
-    class Confetti1Git < Base
+    class ConfettiGit < Base
+
+      def initialize
+        #@ignored = ignored["ignore"]
+      end
 
       def init(git_path, vob_path)
         @git_path = git_path
+        @git_dot_folder = File.join(git_path, ".git") 
         unless Dir.exist? File.join(@git_path, ".git")
           FileUtils.makedirs @git_path
           command("git", "--git-dir=#{@git_path}", "--work-tree=#{vob_path}", "init")
         end
-        @git = Git.init @git_path
+        @git_dot_folder
       end
 
-      def add_all
-        @git.status.changed
-        # @git.add File.join(@path_to_repo, file)
-  
-
-        # @files.each do |view_file|
-        #   path_to_file = File.join @cc_veiw_path, view_file
-        #   path_to_repo_file = File.join @path_to_repo, view_file
-
-        #   #TODO: Not DRY way. 
-        #   # Git gem has :status method, but it works in another way then it described in docs 
-        #   if File.exist? path_to_repo_file
-        #     next if @ignored.include? view_file
-        #     next unless File.exist? path_to_file
-        #     next if FileUtils.compare_file(path_to_file, path_to_repo_file)
-        #     add_file.call(view_file, true)
-        #   end
-
-        #   next unless File.exist? path_to_repo_file
-        #   add_file.call(view_file, false)
-        #   #--- end
-
-        # end
+      def exclude
+        File.open(File.join(@git_dot_folder, 'info', 'exclude'), 'w') do |f|
+          f.puts @ignored.join("\n")
+        end
       end
 
-      def commit!
-        # @git.commit('First commit from script!')
-        # @git_woking_tree_clean = true
-      end 
+      def add(all=false, files=[])
+        command "git", "add", "#{all ? '.' : files.join(' ')}"
+      end
+
+      def commit!(all=true, message="Confetti commit")
+        command "git", "commit", "#{all ? '-a': ''}", "-m\"#{message}\""
+      end
+
+      def status
+        status_mode = lambda do |status, mode|
+          status.select{|o| o =~ /#{mode}\s+/}.map{|o| o.gsub(/#{mode}\s+/, '').gsub(/\s/, '')}
+        end
+        out = command "git", "status", "--porcelain"
+        {
+          untracked:  status_mode.call(out, /\?\?/),
+          modified:   status_mode.call(out, 'M'),
+          deleted:    status_mode.call(out, 'D'),
+          renamed:    status_mode.call(out, 'R'),
+          copied:     status_mode.call(out, 'C')
+        }
+      end  
 
     end
   end
