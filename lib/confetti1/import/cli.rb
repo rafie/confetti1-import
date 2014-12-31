4499c2b73f3212642071797a41cca06ef8324e4f
Vitaliy Trofymyuk <vitaliy.trofymyuk@globallogic.com>
2014-12-31 12:53:59 +0200

    Another movement

--- lib/confetti1/import/cli.rb
+++ lib/confetti1/import/cli.rb
@@ -1,42 +1,44 @@
-module Confetti1Import
-  class CLI < Base
-    
-    def console(*args)
-      require 'confetti1_import'
-      begin
-        require 'pry'
-      rescue LoadError
-        require 'rubygems'
-        require 'pry'
+module Confetti1
+  module Import
+    class CLI < Base
+      
+      def console(*args)
+        require 'confetti1_import'
+        begin
+          require 'pry'
+        rescue LoadError
+          require 'rubygems'
+          require 'pry'
+        end
+        Pry::CLI.parse_options
       end
-      Pry::CLI.parse_options
+
+      alias_method :c, :console
+
+      def init(*argv)
+        Confetti1Import.init
+      end
+
+      def build_versions(*argv)
+        Confetti1Import.build_versions
+      end
+
+      def originate_versions(*argv)
+        Confetti1Import.originate_versions
+      end
+
+      def scan(*argv)
+        Confetti1Import.scan_view
+      end
+
+      def scan_to_yaml(*argv)
+        Confetti1Import.scan_to_yaml
+      end
+
+      def method_missing(meth, *args, &block)
+        puts "Command not found: #{meth}"
+      end
+
     end
-
-    alias_method :c, :console
-
-    def init(*argv)
-      Confetti1Import.init
-    end
-
-    def build_versions(*argv)
-      Confetti1Import.build_versions
-    end
-
-    def originate_versions(*argv)
-      Confetti1Import.originate_versions
-    end
-
-    def scan(*argv)
-      Confetti1Import.scan_view
-    end
-
-    def scan_to_yaml(*argv)
-      Confetti1Import.scan_to_yaml
-    end
-
-    def method_missing(meth, *args, &block)
-      puts "Command not found: #{meth}"
-    end
-
   end
 end
