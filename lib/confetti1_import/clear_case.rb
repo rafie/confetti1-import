c64d8533cdefdecd14c1fd1c86b45c5285033b4b
Vitaliy Trofymyuk <vitaliy.trofymyuk@globallogic.com>
2014-12-31 12:07:34 +0200

    GEM preconfiguration; modules

--- lib/confetti1_import/clear_case.rb
+++ lib/confetti1_import/clear_case.rb
@@ -1,138 +1,140 @@
-module Confetti1Import
-  class ClearCase < Base
-    require_relative "brsource"
-    include Confetti1Import::Brsource
+module Confetti1
+  module Import
+    class ClearCase < Base
+      require_relative "brsource"
+      include Confetti1Import::Brsource
 
-    def initialize
-      @view_path = ConfettiEnv.view_path 
-    end
-
-    def scan
-      big_files = []
-      small_files = []
-      ignored = []
-      current_dir = Dir.getwd
-      ignore_list = ConfettiEnv.ignore_list
-      puts "Scanning view ----> #{@view_path}"
-      Dir.glob(File.join(@view_path, '**', '*')).each do |view_entry|
-        next if File.directory?(view_entry)
-        unless File.exist?(view_entry)
-          puts "File #{view_entry} is not found".red.bold
-          next
-        end
-        unless ignore_list.select{|il| File.fnmatch(File.join(@view_path, il), view_entry)}.empty?
-          ignored << view_entry
-          next
-        end
-        if File.size(view_entry) < ConfettiEnv.exclude_size
-          small_files << view_entry
-        else
-          big_files << view_entry
-        end
-
+      def initialize
+        @view_path = ConfettiEnv.view_path 
       end
-      puts "WARNING: #{ignored.size} files will be ignored:"
-      ignored.each{|i|print "#{i},\s"}
-      puts
-      File.open(File.join(ConfettiEnv.home, 'config', 'small.txt'), 'w'){|f| f.write(small_files.join("\n"))}
-      File.open(File.join(ConfettiEnv.home, 'config', 'big.txt'), 'w'){|f| f.write(big_files.join("\n"))}
-      File.open(File.join(ConfettiEnv.home, 'config', 'ignored.txt'), 'w'){|f| f.write(ignored.join("\n"))}
-      {ignored: ignored, big_files: big_files, small_files: small_files}
-    end
 
-    def scan_to_yaml
-      big_files = {}
-      small_files = {}
-      ignored = []
-
-      current_dir = ConfettiEnv.home
-      ignore_list = ConfettiEnv.ignore_list
-
-      configspec = self.configspec
-
-      configspec.each do |cs|
-        current_version = "#{rand(10000)}_vob_#{cs[:vob]}_#{cs[:version]}"
-        big_vob_files = []
-        small_vob_files = []
-
-        Dir.glob(File.join(cs[:path], '**', '*')).each do |vob_entry|
-          next if File.directory?(vob_entry)
-
-          unless File.exist?(vob_entry)
-            puts "File #{vob_entry} is not found".red.bold
+      def scan
+        big_files = []
+        small_files = []
+        ignored = []
+        current_dir = Dir.getwd
+        ignore_list = ConfettiEnv.ignore_list
+        puts "Scanning view ----> #{@view_path}"
+        Dir.glob(File.join(@view_path, '**', '*')).each do |view_entry|
+          next if File.directory?(view_entry)
+          unless File.exist?(view_entry)
+            puts "File #{view_entry} is not found".red.bold
             next
           end
-
-          unless ignore_list.select{|il| File.fnmatch(File.join(@view_path, il), vob_entry)}.empty?
-            ignored << vob_entry
+          unless ignore_list.select{|il| File.fnmatch(File.join(@view_path, il), view_entry)}.empty?
+            ignored << view_entry
             next
           end
-
-          if File.size(vob_entry) < ConfettiEnv.exclude_size
-            small_vob_files << vob_entry
+          if File.size(view_entry) < ConfettiEnv.exclude_size
+            small_files << view_entry
           else
-            big_vob_files << vob_entry
+            big_files << view_entry
           end
 
-        end #globb
-        big_files.merge!(current_version => big_vob_files) unless big_vob_files.empty?
-        small_files.merge!(current_version => small_vob_files) unless small_vob_files.empty?
-      end# configspec
-
-      File.open(File.join(ConfettiEnv.home, 'config', 'small.yml'), 'w'){|f| f.write(small_files.to_yaml)}
-      File.open(File.join(ConfettiEnv.home, 'config', 'big.yml'), 'w'){|f| f.write(big_files.to_yaml)}
-      File.open(File.join(ConfettiEnv.home, 'config', 'ignored.yml'), 'w'){|f| f.write(ignored.to_yaml)}
-    end
-
-
-    def configspec(parse_file=nil)
-      out=""
-      if parse_file
-        out = File.read(parse_file).split("\n")
-      else
-        in_directory(@view_path){out = ct "catcs"}
+        end
+        puts "WARNING: #{ignored.size} files will be ignored:"
+        ignored.each{|i|print "#{i},\s"}
+        puts
+        File.open(File.join(ConfettiEnv.home, 'config', 'small.txt'), 'w'){|f| f.write(small_files.join("\n"))}
+        File.open(File.join(ConfettiEnv.home, 'config', 'big.txt'), 'w'){|f| f.write(big_files.join("\n"))}
+        File.open(File.join(ConfettiEnv.home, 'config', 'ignored.txt'), 'w'){|f| f.write(ignored.join("\n"))}
+        {ignored: ignored, big_files: big_files, small_files: small_files}
       end
-      parse_configspec out
-    end
 
-    def configspec=(cs_file)
-      out = ""
-      in_directory(@view_path){out = ct("setcs", cs_file)}
-      raise out if out =~ /^cleartool\:\sError\:/
-    end
+      def scan_to_yaml
+        big_files = {}
+        small_files = {}
+        ignored = []
 
-    def inside_view(*params, &block)
-      raise 'No block given' unless block_given?
-      in_directory(@view_path) do
-        yield
+        current_dir = ConfettiEnv.home
+        ignore_list = ConfettiEnv.ignore_list
+
+        configspec = self.configspec
+
+        configspec.each do |cs|
+          current_version = "#{rand(10000)}_vob_#{cs[:vob]}_#{cs[:version]}"
+          big_vob_files = []
+          small_vob_files = []
+
+          Dir.glob(File.join(cs[:path], '**', '*')).each do |vob_entry|
+            next if File.directory?(vob_entry)
+
+            unless File.exist?(vob_entry)
+              puts "File #{vob_entry} is not found".red.bold
+              next
+            end
+
+            unless ignore_list.select{|il| File.fnmatch(File.join(@view_path, il), vob_entry)}.empty?
+              ignored << vob_entry
+              next
+            end
+
+            if File.size(vob_entry) < ConfettiEnv.exclude_size
+              small_vob_files << vob_entry
+            else
+              big_vob_files << vob_entry
+            end
+
+          end #globb
+          big_files.merge!(current_version => big_vob_files) unless big_vob_files.empty?
+          small_files.merge!(current_version => small_vob_files) unless small_vob_files.empty?
+        end# configspec
+
+        File.open(File.join(ConfettiEnv.home, 'config', 'small.yml'), 'w'){|f| f.write(small_files.to_yaml)}
+        File.open(File.join(ConfettiEnv.home, 'config', 'big.yml'), 'w'){|f| f.write(big_files.to_yaml)}
+        File.open(File.join(ConfettiEnv.home, 'config', 'ignored.yml'), 'w'){|f| f.write(ignored.to_yaml)}
       end
-    end
 
-    def originate(label_path)
-      self.configspec = File.expand_path(File.join(label_path, 'configspec.txt'))
-      label = label_path.split(/\/|\\/).last
-      self.inside_view do
-      origin =  `ruby #{ConfettiEnv.home}/brsource.rb mcu_#{label}`
-      puts origin
-      origin
-    end
 
-  private
-
-    def parse_configspec(conf_spec)
-      splited = conf_spec.map{|cs|cs.split("\s")}.reject{|cs| cs.empty? or cs.size < 4 or cs.detect{|ccs| ccs=~/^#/}}
-      splited.map do |cs| 
-        vob_name = cs[1].to_s[/\w{3,}/]
-        {
-          vob: vob_name, 
-          version: cs[2],
-          path: File.join(@view_path, vob_name)
-        } 
+      def configspec(parse_file=nil)
+        out=""
+        if parse_file
+          out = File.read(parse_file).split("\n")
+        else
+          in_directory(@view_path){out = ct "catcs"}
+        end
+        parse_configspec out
       end
-    end
 
-    def ct(*params)
-      command "ct", params.join("\s")
+      def configspec=(cs_file)
+        out = ""
+        in_directory(@view_path){out = ct("setcs", cs_file)}
+        raise out if out =~ /^cleartool\:\sError\:/
+      end
+
+      def inside_view(*params, &block)
+        raise 'No block given' unless block_given?
+        in_directory(@view_path) do
+          yield
+        end
+      end
+
+      def originate(label_path)
+        self.configspec = File.expand_path(File.join(label_path, 'configspec.txt'))
+        label = label_path.split(/\/|\\/).last
+        self.inside_view do
+        origin =  `ruby #{ConfettiEnv.home}/brsource.rb mcu_#{label}`
+        puts origin
+        origin
+      end
+
+    private
+
+      def parse_configspec(conf_spec)
+        splited = conf_spec.map{|cs|cs.split("\s")}.reject{|cs| cs.empty? or cs.size < 4 or cs.detect{|ccs| ccs=~/^#/}}
+        splited.map do |cs| 
+          vob_name = cs[1].to_s[/\w{3,}/]
+          {
+            vob: vob_name, 
+            version: cs[2],
+            path: File.join(@view_path, vob_name)
+          } 
+        end
+      end
+
+      def ct(*params)
+        command "ct", params.join("\s")
+      end
     end
   end
 end
--- lib/confetti1_import/git.rb
+++ lib/confetti1_import/git.rb
@@ -1,103 +1,105 @@
-module Confetti1Import
-  class Git < Base
+module Confetti1
+  module Import
+    class Git < Base
 
-    def initialize(args={})
-      @git_path        = args[:path] || ConfettiEnv.git_path
+      def initialize(args={})
+        @git_path        = args[:path] || ConfettiEnv.git_path
 
-      @git_dot_folder  = File.join(@git_path, '.git')
-      @exclude_file    = File.join(@git_dot_folder, 'info', 'exclude')
-      @view_path       = ConfettiEnv.view_path
-      @ignore_list     = ConfettiEnv.ignore_list
-      @exclude_size    = ConfettiEnv.exclude_size
-      @clone_path      = File.join(ConfettiEnv.workspace, 'testing_repo')
-    end
-
-    def init
-      puts "Import started.."
-      unless File.exist?(@exclude_file)
-        puts "Initialing empty git repository.."
-        puts @git_dot_folder
-        FileUtils.mkdir_p(@git_path)
-        git "--git-dir=#{@git_dot_folder} --work-tree=#{@view_path} init"
-        in_directory(@git_path){git 'commit --allow-empty -m"initial commit"'}
+        @git_dot_folder  = File.join(@git_path, '.git')
+        @exclude_file    = File.join(@git_dot_folder, 'info', 'exclude')
+        @view_path       = ConfettiEnv.view_path
+        @ignore_list     = ConfettiEnv.ignore_list
+        @exclude_size    = ConfettiEnv.exclude_size
+        @clone_path      = File.join(ConfettiEnv.workspace, 'testing_repo')
       end
-      puts "Initialized"
-      @git_dot_folder
-    end
 
-    def exclude!(file_list)
-      puts "Excluding files bigger then #{@exclude_size} bites"
-      to_exclude = file_list
-      File.open(@exclude_file, 'w'){|f| f.write(to_exclude.join("\n"))}
-    end
-
-    def commit(file_list, message)
-      in_directory(@git_dot_folder) do
-        file_list.each do |file|
-          puts "Adding #{file}"
-          git "add \"#{file}\""
+      def init
+        puts "Import started.."
+        unless File.exist?(@exclude_file)
+          puts "Initialing empty git repository.."
+          puts @git_dot_folder
+          FileUtils.mkdir_p(@git_path)
+          git "--git-dir=#{@git_dot_folder} --work-tree=#{@view_path} init"
+          in_directory(@git_path){git 'commit --allow-empty -m"initial commit"'}
         end
-        git "commit -m\"#{message}\""
+        puts "Initialized"
+        @git_dot_folder
       end
-    end
 
-    def commit_a!(message)
-      in_directory(@git_dot_folder) do
-        git 'add .'
-        git "commit -m\"#{message}\""
+      def exclude!(file_list)
+        puts "Excluding files bigger then #{@exclude_size} bites"
+        to_exclude = file_list
+        File.open(@exclude_file, 'w'){|f| f.write(to_exclude.join("\n"))}
       end
-    end
 
-    def correct?(file_list, place=nil)
-      puts "Started test"
-      test_clone(place)
-      raise "Repository is not cloned for testing" unless Dir.exist? @cloned_repository
-      small_files = file_list.map{|fl| fl.gsub(@view_path, '')}
-      cloned_files = in_directory(@cloned_repository){command('git ls-files')}.map{|cl| cl.gsub(@cloned_repository, '')}
-
-      
-      puts "Small size: #{small_files.size}"
-      puts "Cloned size: #{cloned_files.size}"
-
-      escaped_small_files = small_files.map{|sf|sf.gsub(/\/|\\/, '')}.sort
-      escaped_cloned_files = cloned_files.map{|cf|cf.gsub(/\/|\\/, '')}.sort
-      correctness = escaped_small_files==escaped_cloned_files
-      if correctness
-        puts "Version imported correctly".green.bold
-      else
-        puts "Version imported not correctly".red.bold
+      def commit(file_list, message)
+        in_directory(@git_dot_folder) do
+          file_list.each do |file|
+            puts "Adding #{file}"
+            git "add \"#{file}\""
+          end
+          git "commit -m\"#{message}\""
+        end
       end
-      correctness
-    end
 
-    def on_branch?(branch)
-      in_directory(@view_root) do
-        current_branch = git("branch").detect{|br| br =~ /^\*/}.gsub(/^\*\s/, "")
-        current_branch == branch
+      def commit_a!(message)
+        in_directory(@git_dot_folder) do
+          git 'add .'
+          git "commit -m\"#{message}\""
+        end
       end
-    end
 
+      def correct?(file_list, place=nil)
+        puts "Started test"
+        test_clone(place)
+        raise "Repository is not cloned for testing" unless Dir.exist? @cloned_repository
+        small_files = file_list.map{|fl| fl.gsub(@view_path, '')}
+        cloned_files = in_directory(@cloned_repository){command('git ls-files')}.map{|cl| cl.gsub(@cloned_repository, '')}
 
-  private
+        
+        puts "Small size: #{small_files.size}"
+        puts "Cloned size: #{cloned_files.size}"
 
-    def clean_up!
-      FileUtils.rm_rf @cloned_repository
-    end
-
-    def test_clone(path=nil)
-      @cloned_repository = File.join([ConfettiEnv.clone_path, path].compact)
-      git "clone", @git_dot_folder, @cloned_repository
-    end
-
-    def status
-      output = in_directory(@view_path){git('status --porcelain').map{|st| st =~ /^\s\w\s/}}
-    end
-
-    def git(*params)
-      in_directory(@git_path) do
-        command 'git', params.join("\s")
+        escaped_small_files = small_files.map{|sf|sf.gsub(/\/|\\/, '')}.sort
+        escaped_cloned_files = cloned_files.map{|cf|cf.gsub(/\/|\\/, '')}.sort
+        correctness = escaped_small_files==escaped_cloned_files
+        if correctness
+          puts "Version imported correctly".green.bold
+        else
+          puts "Version imported not correctly".red.bold
+        end
+        correctness
       end
-    end
 
+      def on_branch?(branch)
+        in_directory(@view_root) do
+          current_branch = git("branch").detect{|br| br =~ /^\*/}.gsub(/^\*\s/, "")
+          current_branch == branch
+        end
+      end
+
+
+    private
+
+      def clean_up!
+        FileUtils.rm_rf @cloned_repository
+      end
+
+      def test_clone(path=nil)
+        @cloned_repository = File.join([ConfettiEnv.clone_path, path].compact)
+        git "clone", @git_dot_folder, @cloned_repository
+      end
+
+      def status
+        output = in_directory(@view_path){git('status --porcelain').map{|st| st =~ /^\s\w\s/}}
+      end
+
+      def git(*params)
+        in_directory(@git_path) do
+          command 'git', params.join("\s")
+        end
+      end
+
+    end
   end
 end
